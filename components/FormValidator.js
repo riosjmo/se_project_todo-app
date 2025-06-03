0fe45cf2359c20dbb861634e574ab9508b07242c
rio <riojordan127@yahoo.com>
2025-06-03 13:13:14 -0500

    form validator pt. 2 finished project

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,6 +1,7 @@
 class FormValidator {
   constructor(settings, formEl) {
     this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
     this._submitButtonSelector = settings.submitButtonSelector;
     this._errorClass = settings.errorClass;
     this._inputErrorClass = settings.inputErrorClass;
@@ -8,7 +9,58 @@ class FormValidator {
     this._formEl = formEl;
   }
 
-  enableValidation() {}
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+    showInputError(
+      this._formEl,
+      inputElement,
+      inputElement.validationMessage,
+    );
+  } else {
+    hideInputError(inputElement);
+  }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+  }
+
+  _toggleButtonState(buttonElement) {
+    if (this._hasInvalidInput()) {
+    buttonElement.classList.add(this._inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(this._inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+    this._formEl.querySelectorAll(this._inputSelector),
+  );
+  const buttonElement = this._formEl.querySelector(
+    this._submitButtonSelector,
+  );
+
+  this._toggleButtonState(buttonElement);
+
+  this._inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      this._checkInputValidity(inputElement);
+      this._toggleButtonState(buttonElement);
+    });
+  });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 
 export default FormValidator;
