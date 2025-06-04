22c1ed2df42c4a5c7f3f53c5a862b6af4fe86ffc
rio <riojordan127@yahoo.com>
2025-06-04 12:08:58 -0500

    polish project for second submission

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -9,50 +9,62 @@ class FormValidator {
     this._formEl = formEl;
   }
 
+  _showInputError(inputElement, errorMessage) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = this._errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElementId = `#${inputElement.id}-error`;
+    const errorElement = this._formEl.querySelector(errorElementId);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
+
   _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
-    showInputError(
-      this._formEl,
-      inputElement,
-      inputElement.validationMessage,
-    );
-  } else {
-    hideInputError(inputElement);
-  }
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
   }
 
   _hasInvalidInput() {
     return this._inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
+      return !inputElement.validity.valid;
+    });
   }
 
   _toggleButtonState(buttonElement) {
     if (this._hasInvalidInput()) {
-    buttonElement.classList.add(this._inactiveButtonClass);
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove(this._inactiveButtonClass);
-    buttonElement.disabled = false;
-  }
+      buttonElement.classList.add(this._inactiveButtonClass);
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove(this._inactiveButtonClass);
+      buttonElement.disabled = false;
+    }
   }
 
   _setEventListeners() {
     this._inputList = Array.from(
-    this._formEl.querySelectorAll(this._inputSelector),
-  );
-  const buttonElement = this._formEl.querySelector(
-    this._submitButtonSelector,
-  );
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    const buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
 
-  this._toggleButtonState(buttonElement);
+    this._toggleButtonState(buttonElement);
 
-  this._inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", () => {
-      this._checkInputValidity(inputElement);
-      this._toggleButtonState(buttonElement);
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(buttonElement);
+      });
     });
-  });
   }
 
   enableValidation() {
